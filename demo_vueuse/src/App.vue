f5c34312f23e8dc51f99889fc67497e292edb14b
KeeneChen <keenechen@qq.com>
2024-01-15 20:22:04 +0800

    feat: 添加 vueuse 使用示例项目

---
+++ demo_vueuse/src/App.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header>
+    <router-link to="/">Home</router-link>
+    <router-link to="/event_bus/emit">Emit</router-link>
+    <router-link to="/event_bus/on">On</router-link>
+    <router-link to="/event_bus/once">Once</router-link>
+  </header>
+
+  <router-view />
+</template>
+
+<style lang="scss" scoped>
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    gap: 20px;
+    padding: 20px;
+    font-size: 20px;
+    font-weight: 500;
+    color: #3b4cd1;
+  }
+
+  a {
+    text-decoration: none;
+    border-bottom: transparent 3px solid;
+    color: #ccc;
+
+    &:hover {
+      border-bottom: #3b4cd1 3px solid;
+      color: #3b4cd1;
+      transition: all 0.3s ease;
+      transform: scale(1.1);
+    }
+  }
+</style>
---
+++ demo_vueuse/src/views/event_bus/Emit.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  import { useEventBus } from '@vueuse/core';
+
+  const myEventBus = useEventBus<string>('my-event-bus');
+
+  const handleClick = () => {
+    console.log('Emitting event...');
+    
+    myEventBus.emit('Hello World!');
+  };
+</script>
+
+<template>
+  <button @click="handleClick">Emit</button>
+</template>
