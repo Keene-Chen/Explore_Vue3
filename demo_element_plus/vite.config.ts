176c51d779e6b36e8de0473e80e4f4194e85dd77
KeeneChen <keenechen@qq.com>
2024-01-21 21:24:41 +0800

    chore: 添加 vite-plugin-mock-dev-server 库

--- demo_element_plus/vite.config.ts
+++ demo_element_plus/vite.config.ts
@@ -1,10 +1,11 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import mockDevServerPlugin from 'vite-plugin-mock-dev-server';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), mockDevServerPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('src', import.meta.url)),
