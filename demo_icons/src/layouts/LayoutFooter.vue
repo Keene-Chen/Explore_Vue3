439937ddcb2a66294dbdb8f26ac5480e0ac0264a
KeeneChen <keenechen@qq.com>
2024-02-01 13:33:16 +0800

    refactor(demo_icons): 修改布局样式

---
+++ demo_icons/src/layouts/LayoutFooter.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <n-layout-footer bordered>尾部</n-layout-footer>
+</template>
+
+<style scoped>
+.n-layout-footer {
+  background: #000;
+  padding: 12px 10px;
+}
+</style>
