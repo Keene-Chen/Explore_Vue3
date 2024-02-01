a81787a21d9bb9713069b73aa64712223551f7b2
KeeneChen <keenechen@qq.com>
2024-02-01 13:36:00 +0800

    refactor(demo_icons): 修改页面组件

---
+++ demo_icons/src/views/icons/lucide/IndexView.vue
@@ -0,0 +1,95 @@
+<script lang="ts" setup>
+import * as icons from 'lucide-vue-next';
+import { useMessage } from 'naive-ui';
+import { computed, reactive, ref } from 'vue';
+
+const iconObj = reactive({
+  size: 24,
+  color: '#E8E8E8',
+  strokeWidth: 2,
+  defaultClass: 'icon',
+});
+
+// 每页显示的图标数
+const iconsPerPage = 162;
+
+// 当前页码
+const currentPage = ref(1);
+// 总页码，使用 Math.ceil 来确保总是向上取整
+const totalPage = computed(() => Math.ceil(Object.entries(icons).length / iconsPerPage));
+
+// 计算当前页的图标
+const iconList = computed(() => {
+  // 计算当前页面应显示的图标范围
+  const startIndex = (currentPage.value - 1) * iconsPerPage;
+  const endIndex = startIndex + iconsPerPage;
+
+  // 返回当前页面的图标
+  return Object.entries(icons)
+    .slice(startIndex, endIndex)
+    .map(([name, component]) => ({ name, component }));
+});
+
+const handleCopy = async (name: string) => {
+  try {
+    await navigator.clipboard.writeText(name);
+    console.log(`Copied: ${name}`);
+    useMessage().warning(`Copied: ${name}`);
+  } catch (err) {
+    console.error('Failed to copy text: ', err);
+  }
+};
+</script>
+
+<template>
+  <n-flex justify="start">
+    <n-card>
+      <n-flex vertical>
+        <span>自定义</span>
+        <n-button>恢复</n-button>
+        <span>Color</span>
+        <n-color-picker
+          v-model:value="iconObj.color"
+          :swatches="['#FFFFFF', '#18A058', '#2080F0', '#F0A020', 'rgba(208, 48, 80, 1)']"
+        >
+        </n-color-picker>
+        <span>Stroke width {{ iconObj.strokeWidth }}px</span>
+        <n-slider v-model:value="iconObj.strokeWidth" :step="0.25" :min="0.5" :max="3" />
+        <span>Size width {{ iconObj.size }}px</span>
+        <n-slider v-model:value="iconObj.size" :step="4" :min="16" :max="48" />
+      </n-flex>
+    </n-card>
+    <template v-for="{ name, component } in iconList" :key="name">
+      <n-tooltip placement="bottom" trigger="hover">
+        <template #trigger>
+          <n-button tertiary type="primary" size="large" @click="handleCopy(name)">
+            <component
+              :is="component"
+              :size="iconObj.size"
+              :color="iconObj.color"
+              :stroke-width="iconObj.strokeWidth"
+              :class="iconObj.defaultClass"
+            ></component>
+          </n-button>
+        </template>
+        <span>{{ name }}</span>
+      </n-tooltip>
+    </template>
+  </n-flex>
+  <n-divider />
+  <n-flex justify="center">
+    <n-pagination
+      v-model:page="currentPage"
+      :page-count="totalPage"
+      size="large"
+      show-quick-jumper
+      show-size-picker
+    />
+  </n-flex>
+</template>
+
+<style scoped>
+.n-card {
+  max-width: 200px;
+}
+</style>
---
+++ demo_icons/src/views/icons/lucide/LucideIconView.vue
@@ -0,0 +1,94 @@
+<script lang="ts" setup>
+import * as icons from 'lucide-vue-next';
+import {
+NFlex,
+NLayout,
+NLayoutContent,
+NLayoutFooter,
+NLayoutHeader,
+NLayoutSider,
+} from 'naive-ui';
+import { computed, ref } from 'vue';
+
+const loading = ref<boolean>(true);
+const page = ref<number>(2);
+
+const props = defineProps<{
+  size?: number;
+  color?: string;
+  strokeWidth?: number;
+  defaultClass?: string;
+}>();
+
+const iconList = computed(() =>
+  Object.entries(icons).map(([name, component]) => ({ name, component }))
+);
+</script>
+
+<template>
+  <n-layout style="height: 100vh; width: 100vw">
+    <n-layout-header position="absolute" style="height: 64px; padding: 24px" bordered>
+      颐和园路
+    </n-layout-header>
+
+    <n-layout position="absolute" style="top: 64px" has-sider>
+      <n-layout-sider content-style="padding: 24px;" :native-scrollbar="false" bordered>
+        <template v-for="item in 20" :key="item">
+          <n-h2>{{ item }}</n-h2>
+        </template>
+      </n-layout-sider>
+
+      <n-layout-content content-style="padding: 10px;">
+        <n-flex justify="start" :size="[20, 20]">
+          <template v-for="item in 25" :key="item">
+            <n-tooltip placement="bottom" trigger="hover">
+              <template #trigger>
+                <n-button strong secondary type="primary">{{ item }}</n-button>
+              </template>
+              <span>{{ item }}</span>
+            </n-tooltip>
+          </template>
+          <!--   <template v-for="{ name, component } in iconList" :key="name">
+            <component
+              :is="component"
+              :size="props.size"
+              :color="props.color"
+              :stroke-width="props.strokeWidth"
+              :class="props.defaultClass"
+            />
+          </template> -->
+        </n-flex>
+        <n-divider />
+        <n-flex justify="center">
+          <n-pagination
+            v-model:page="page"
+            :page-count="100"
+            size="large"
+            show-quick-jumper
+            show-size-picker
+          />
+        </n-flex>
+      </n-layout-content>
+    </n-layout>
+
+    <n-layout-footer position="absolute" style="height: 64px; padding: 12px" bordered>
+      城府路56456456
+    </n-layout-footer>
+  </n-layout> 
+</template>
+
+<style scoped>
+.n-layout-header,
+.n-layout-footer {
+  background: #773737;
+  padding: 12px 10px;
+}
+
+.n-layout-sider {
+  background: #773737;
+}
+
+.n-layout-content {
+  background: #2aa97643;
+}
+</style>
