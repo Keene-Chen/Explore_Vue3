0ebce5a639a3cb14870e3960f391cc9ed1c842eb
KeeneChen <keenechen@qq.com>
2024-02-01 13:35:02 +0800

    refactor(demo_icons): 修改路由配置

--- demo_icons/src/router/index.ts
+++ demo_icons/src/router/index.ts
@@ -1,3 +1,4 @@
+import IndexView from '@/layouts/IndexView.vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
@@ -5,24 +6,31 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'layout',
-      component: () => import('@/layout/BaseLayout.vue'),
+      meta: {
+        layout: IndexView,
+        title: '首页',
+      },
+      component: IndexView,
       children: [
         {
           path: '',
           name: 'home',
-          component: () => import('@/views/HomeView.vue'),
+          component: () => import('@/views/icons/lucide/IndexView.vue'),
+        },
+        {
+          path: '/lucide',
+          name: 'lucide',
+          component: () => import('@/views/icons/lucide/LucideIconView.vue'),
         },
       ],
     },
     {
-      path: '/lucide',
-      name: 'lucide',
-      component: () => import('@/views/LucideIconView.vue'),
-    },
-    {
       path: '/404',
       name: '404',
+      meta: {
+        layout: '404',
+        title: '404',
+      },
       component: () => import('@/views/error/404View.vue'),
     },
   ],
