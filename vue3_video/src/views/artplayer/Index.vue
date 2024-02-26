6d297db9c97048a9e6ef29b523761cc610c0415a
KeeneChen <keenechen@qq.com>
2024-02-26 14:52:21 +0800

    refactor: 统一导出 artplayer 测试组件

--- vue3_video/src/views/artplayer/Index.vue
+++ vue3_video/src/views/artplayer/Index.vue
@@ -1,41 +1,45 @@
 <script lang="ts" setup>
 import { NBadge, NGi, NGrid, NSpace } from 'naive-ui';
-import BaseVideo from './components/BaseVideo.vue';
-import LiveVideo from './components/LiveVideo.vue';
-import Video2 from './components/Video2.vue';
-import Video3 from './components/Video3.vue';
+import { BaseVideo, BaseVideo2, Flv, LiveVideo, M3U8, Video2, Video3, VideoTheme } from './components/videoCompnent';
+
+// 定义组件数组
+const components = [
+  { id: 1, name: 'BaseVideo', component: BaseVideo },
+  { id: 2, name: 'BaseVideo2', component: BaseVideo2 },
+  { id: 3, name: 'Flv', component: Flv },
+  { id: 4, name: 'LiveVideo', component: LiveVideo },
+  { id: 5, name: 'M3U8', component: M3U8 },
+  { id: 6, name: 'Video2', component: Video2 },
+  { id: 7, name: 'Video3', component: Video3 },
+  { id: 8, name: 'VideoTheme', component: VideoTheme },
+  { id: 9, name: 'VideoTheme', component: VideoTheme },
+  { id: 10, name: 'VideoTheme', component: VideoTheme },
+  { id: 11, name: 'VideoTheme', component: VideoTheme },
+];
 </script>
 
 <template>
   <n-space vertical size="large">
     <n-grid x-gap="0" y-gap="0" :cols="3">
-      <n-gi class="warp">
-        <n-badge value="新sadasdsadada" />
-        <base-video />
-      </n-gi>
-      <n-gi class="warp">
-        <video2 />
-      </n-gi>
-      <n-gi class="warp">
-        <video3 />
-      </n-gi>
-      <n-gi class="warp">
-        <live-video />
-      </n-gi>
-      <n-gi class="warp"> </n-gi>
-      <n-gi class="warp"> </n-gi>
-      <n-gi class="warp"> </n-gi>
-      <n-gi class="warp"> </n-gi>
-      <n-gi class="warp"> </n-gi>
-      <n-gi class="warp"> </n-gi>
+      <template v-for="item in components" :key="item.id">
+        <n-gi class="warp">
+          <n-badge :value="item.name" type="info" />
+          <component :is="item.component" />
+        </n-gi>
+      </template>
     </n-grid>
   </n-space>
 </template>
 
 <style scoped>
-.n-badge{
+.n-badge {
   position: absolute;
-  z-index: 9999;
+  z-index: 99;
+}
+:deep(.n-badge-sup) {
+  border-radius: 0px;
+  border-top-left-radius: 4px;
+  border-bottom-right-radius: 4px;
 }
 .warp {
   height: calc(100vh / 3);
